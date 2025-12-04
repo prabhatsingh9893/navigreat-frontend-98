f69cabca8feca889d47d4581b1ce0df6a75973b0
Aapka GitHub Naam <aapka-asli-email@gmail.com>
2025-12-04 12:00:16 +0530

    Updated Real Firebase Config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // 👇 अपना Firebase वाला कोड यहाँ पेस्ट करें 👇
 const firebaseConfig = {
-  apiKey: "AIzaSyD...",
-  authDomain: "edumentor-auth.firebaseapp.com",
-  projectId: "edumentor-auth",
-  storageBucket: "edumentor-auth.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDeptcTFcA7-3l8mRmgWKdcAtme_t-YIHo",
+  authDomain: "edumentor-auth-37712.firebaseapp.com",
+  projectId: "edumentor-auth-37712",
+  storageBucket: "edumentor-auth-37712.firebasestorage.app",
+  messagingSenderId: "414293925328",
+  appId: "1:414293925328:web:4a503e0e68eec34d93add5"
 };
 
 const app = initializeApp(firebaseConfig);
