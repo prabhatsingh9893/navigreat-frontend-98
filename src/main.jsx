bfdff9d03ffb83933b8a91476ef7cb21eab6450b
Aapka GitHub Naam <aapka-asli-email@gmail.com>
2025-12-21 23:22:59 +0530

    Update deployment config for new repository

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
-// Polyfill Globals for Zoom SDK (Required for v3.1.6)
+// Polyfill Globals for Zoom SDK (Required for v3.1.6+)
 import _ from 'lodash';
 import * as Redux from 'redux';
 import { Buffer } from 'buffer';
@@ -17,8 +17,8 @@ window.Redux = Redux;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   //</React.StrictMode>,
 )
