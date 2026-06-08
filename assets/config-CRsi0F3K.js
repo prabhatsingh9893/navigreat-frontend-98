5aa3da60d9744996c9b33a7c79f58498b66848d1
Aapka GitHub Naam <aapka-asli-email@gmail.com>
2026-06-08 13:25:08 +0530

    Updates

---
+++ assets/config-CRsi0F3K.js
@@ -0,0 +1,1 @@
+const o=window.location.hostname==="localhost"||window.location.hostname==="127.0.0.1"||window.location.hostname.startsWith("192.168.")||window.location.hostname.startsWith("10."),t=o?"/api":"https://navigreat-backend-98.onrender.com/api";export{t as A};
